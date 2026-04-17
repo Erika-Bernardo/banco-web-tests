6def4af0364e920ce61416aab6164c8c6b42e37a
Erika-Bernardo <erika_bj_kk@hotmail.com>
2026-04-17 17:50:08 -0300

    Adicionando o relatorio html do mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4000',
-    video: true,
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
