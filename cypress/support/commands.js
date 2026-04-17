f7251f3088874b7440eecc3a2af31d08c08e9953
Erika-Bernardo <erika_bj_kk@hotmail.com>
2026-04-17 17:40:29 -0300

    implementando comandos customizados para login, tranferencias e commons

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+//comando customizado para interagir com a aplicação
+import './commands/common'
+
+// comando customizados para interagir com funcionalidades específicas
+import './commands/login'
+import './commands/transferencia'
